6abb66473e68e6b656e6f9c48ff918ff6e74ebb5
iandoesallthethings <denkyuu@gmail.com>
2022-04-12 21:43:56 -0400

    Attempt to fix formatting on array list. Fail miserably.

--- src/historyStore/ArrayList.svelte
+++ src/historyStore/ArrayList.svelte
@@ -6,7 +6,7 @@
 
 <h4>{label}</h4>
 
-<ul class="w-min flex items-center justify-items-center flex-col ">
+<ul class="w-min flex items-center justify-items-center flex-col">
 	{#each arr as element, i}
 		<li class:list-disc={current === i}>
 			{JSON.stringify(element)}
